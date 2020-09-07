190410cdbe12490b9bd5d5fd2a27d44c8b65d587
Florence Loy <florencelry@gmail.com>
2020-09-07 16:59:27 +0800

    completed longest word game score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,14 +5,31 @@ class GamesController < ApplicationController
     def new
         @alphabets = ('A'..'Z').to_a
         @letters = @alphabets.sample(9)
+        if session[:score] == nil 
+            session[:score] = 0
+        end
     end
 
     def score 
+        # variables
         @letters = params[:letters]
         @letters_string = @letters.gsub(' ', '')
         @answer = params[:answer].upcase
+
+        # parsing json
         url = "https://wagon-dictionary.herokuapp.com/#{@answer.downcase.strip}"
         @heruko = open(url).read
         @word = JSON.parse(@heruko)
+
+        # display results
+        if (@answer.split('').all?(/[#{@letters_string}]/)) == false
+          @not_in_array = true
+        elsif @word["found"] == false
+          @not_true_word = true
+        else
+          @score = @answer.split('').size
+          session[:score] += @score
+          @correct_word = true
+        end   
     end
 end
